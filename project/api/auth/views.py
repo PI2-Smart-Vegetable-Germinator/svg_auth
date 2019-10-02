f7ef2c589d8442a26d579b8128a19537a9b36b21
Felipe Hargreaves <fhargreaves00@gmail.com>
2019-10-02 17:51:59 -0300

    Add route for signing an user up.

--- project/api/auth/views.py
+++ project/api/auth/views.py
@@ -1,6 +1,14 @@
 from flask import Blueprint
 from flask import jsonify
+from flask import request
+from flask import url_for
 
+from flask_bcrypt import generate_password_hash
+
+from marshmallow.exceptions import ValidationError
+
+from project import db
+from .schemas import SignupSchema
 
 auth_blueprint = Blueprint('auth', __name__)
 
@@ -10,3 +18,22 @@ def ping():
     return jsonify({
         'response': 'pong!'
     }), 200
+
+
+@auth_blueprint.route('/api/signup', methods=['POST'])
+def signup():
+    post_data = request.get_json()
+    schema = SignupSchema()
+
+    try:
+        user = schema.load(post_data.get('user'))
+        user.password = generate_password_hash(user.password)
+    except ValidationError as e:
+        return jsonify({'errors': e.normalized_messages()}), 400
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({
+        'success': True,
+    }), 201
