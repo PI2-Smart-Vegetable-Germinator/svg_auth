2b309996260d6fa7f310fc4ba9da1be780dbd6a2
Felipe Hargreaves <fhargreaves00@gmail.com>
2019-10-02 20:16:18 -0300

    Fix an encoding bug in signup route and add login route.

--- project/api/auth/views.py
+++ project/api/auth/views.py
@@ -3,11 +3,13 @@ from flask import jsonify
 from flask import request
 from flask import url_for
 
+from flask_bcrypt import check_password_hash
 from flask_bcrypt import generate_password_hash
 
 from marshmallow.exceptions import ValidationError
 
 from project import db
+from .models import Users
 from .schemas import SignupSchema
 
 auth_blueprint = Blueprint('auth', __name__)
@@ -26,8 +28,9 @@ def signup():
     schema = SignupSchema()
 
     try:
+        # We need to decode the hash before saving it because of encoding issues with postgres.
         user = schema.load(post_data.get('user'))
-        user.password = generate_password_hash(user.password)
+        user.password = generate_password_hash(user.password).decode('utf-8')
     except ValidationError as e:
         return jsonify({'errors': e.normalized_messages()}), 400
 
@@ -36,4 +39,23 @@ def signup():
 
     return jsonify({
         'success': True,
+        'userId': user.id
     }), 201
+
+
+@auth_blueprint.route('/api/login', methods=['POST'])
+def login():
+    post_data = request.get_json()
+
+    user = Users.query.filter_by(email=post_data['email']).first()
+
+    if user and check_password_hash(user.password, post_data['password']):
+        return jsonify({
+            'success': True,
+            'userId': user.id
+        }), 201
+
+    return jsonify({
+        'success': False,
+        'message': 'Invalid e-mail or password'
+    }), 401
